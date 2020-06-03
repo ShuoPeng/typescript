f66fb077697ed7c1f7bd5f0727a59b935bbd8f8c
Shuo Peng <pengshuo2014@gmail.com>
2020-06-02 22:06:31 -0700

    tuple

--- app.js
+++ app.js
@@ -8,12 +8,19 @@
 var person = {
     name: "Shuo",
     age: 32,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+person.role = [1, 'haa'];
+person.role.push('asd'); // push is an expection here;
 var favoriteActivities;
 // or : any [];
 favoriteActivities = ['Sports', 32];
+var a = [1, 2, 3];
+console.log(typeof a);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); 
+    // will get an error, and it's good;
 }
--- app.ts
+++ app.ts
@@ -6,16 +6,28 @@
 //   age: 32,
 // };
 
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
   name: "Shuo",
   age: 32,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 };
+
+person.role = [1, 'haa'];
+person.role.push('asd');// push is an expection here;
 
 let favoriteActivities: (string | number)[];
 // or : any [];
 favoriteActivities = ['Sports', 32];
 
+const a = [1, 2, 3];
+
+console.log(typeof a);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); 
