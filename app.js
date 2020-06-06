45cf273750da6439b810644f39fc6395d326f8df
Shuo Peng <pengshuo2014@gmail.com>
2020-06-06 12:04:10 -0700

    es6

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 "use strict";
-var userName = 'Shuoss';
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+var userName = "Shuoss";
 console.log(userName);
+// default paramters:
+var defaultParameterTestFunction = function (a, b) {
+    if (b === void 0) { b = 1; }
+    return a + b;
+};
+// const defaultParameterTestFunction = (a:number = 1, b:number) => {
+//     console.log(a);
+//     console.log(b);
+// }
+console.log(defaultParameterTestFunction(3));
+// array destructuring 
+var deArray = ['shuo', 32, true];
+var myName = deArray[0], myAge = deArray[1], amIcool = deArray[2], more = deArray.slice(3);
+console.log(myName, myAge, amIcool, more);
+var deObject = {
+    firstName: 'Shuo',
+    age: 32,
+    sex: 'male'
+};
+var username = deObject.firstName, age = deObject.age, rest = __rest(deObject, ["firstName", "age"]);
+console.log(username, age, rest);
--- app.ts
+++ app.ts
@@ -1,3 +1,31 @@
-const userName = 'Shuoss';
+const userName = "Shuoss";
 
 console.log(userName);
+
+// default paramters:
+
+const defaultParameterTestFunction = (a: number, b: number = 1) => a + b;
+
+// const defaultParameterTestFunction = (a:number = 1, b:number) => {
+//     console.log(a);
+//     console.log(b);
+// }
+
+console.log(defaultParameterTestFunction(3));
+
+// array destructuring 
+
+const deArray = ['shuo', 32, true];
+
+let [ myName, myAge, amIcool, ...more ] = deArray;
+
+console.log(myName, myAge, amIcool, more);
+
+const deObject = {
+    firstName: 'Shuo',
+    age: 32,
+    sex: 'male'
+}
+
+let {firstName: username, age, ...rest} = deObject;
+console.log(username, age, rest);
